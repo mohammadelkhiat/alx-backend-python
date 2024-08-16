c5bfc7c5d2971a82fe6358699f0bb3b30fd9c6c5
Mohammad <133120045+mohammadelkhiat@users.noreply.github.com>
2024-08-17 02:41:25 +0300

    Add files via upload

--- 0x00-python_variable_annotation/0-add.py
+++ 0x00-python_variable_annotation/0-add.py
@@ -1,1 +1,7 @@
+#!/usr/bin/env python3
+''' Basic annotations '''
 
+
+def add(a: float, b: float) -> float:
+    ''' return the sum '''
+    return a + b
---
+++ 0x00-python_variable_annotation/README.md
@@ -0,0 +1,6 @@
+#0x00-python_variable_annotations
+
+
+##task 0: 0-add.py
+
+Basic annotations - add
